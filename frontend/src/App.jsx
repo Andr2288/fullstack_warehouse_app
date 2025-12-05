4aee70f045e334f0263c449a1e072e9431f63a3a
chorn <andriychornookyi228@gmail.com>
2025-12-05 22:09:10 +0200

    Initial v2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,6 @@ import Navbar from "./components/Navbar.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
-import SettingsPage from "./pages/SettingsPage.jsx";
-import ProfilePage from "./pages/ProfilePage.jsx";
 
 import {useAuthStore} from "./store/useAuthStore.js";
 import {useEffect} from "react";
@@ -40,8 +38,6 @@ const App = () => {
                 <Route path="/" element={ authUser ? <HomePage /> : <Navigate to="/login" /> } />
                 <Route path="/signup" element={ !authUser ? <SignUpPage /> : <Navigate to="/" /> } />
                 <Route path="/login" element={ !authUser ? <LoginPage /> : <Navigate to="/" /> } />
-                <Route path="/settings" element={<SettingsPage />} />
-                <Route path="/profile" element={ authUser ? <ProfilePage /> : <Navigate to="/login" /> } />
             </Routes>
         </div>
     )
